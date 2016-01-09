87e214a9c3dd02df4878987c9555b3cd00c6a2bf
Fayaz Yusuf Khan <fayaz@weavedin.com>
2016-01-09 12:52:32 +0530

    adding user login

--- reverse_hn_feed/app.py
+++ reverse_hn_feed/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_security import UserMixin, Security, SQLAlchemyUserDataStore
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reverse_hn_feed.db'
+app.config['SECRET_KEY'] = 'secret key'
 db = SQLAlchemy(app)
 admin = Admin(app, name='reverse_hn_feed')
 
@@ -19,6 +21,14 @@ class NewsItem(db.Model):
     upvotes = db.Column(db.Integer)
 
 
+class User(db.Model, UserMixin):
+
+    user_name = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String)
+
+
+user_datastore = SQLAlchemyUserDataStore(db, User)
+security = Security(app, user_datastore)
 admin.add_view(ModelView(NewsItem, db.session))
 
 if __name__ == '__main__':
