eb8265172853b0e97a07c4ad51810332d32559d6
Fayaz Yusuf Khan <fayaz@weavedin.com>
2016-01-09 13:19:05 +0530

    user login features

--- reverse_hn_feed/app.py
+++ reverse_hn_feed/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from flask_security import UserMixin, Security, SQLAlchemyUserDataStore
+from flask_security import (
+    RoleMixin, Security, SQLAlchemyUserDatastore, UserMixin)
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reverse_hn_feed.db'
 app.config['SECRET_KEY'] = 'secret key'
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECURITY_CONFIRMABLE'] = False
 db = SQLAlchemy(app)
 admin = Admin(app, name='reverse_hn_feed')
 
@@ -21,15 +25,28 @@ class NewsItem(db.Model):
     upvotes = db.Column(db.Integer)
 
 
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id')))
+
+
+class Role(db.Model, RoleMixin):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    description = db.Column(db.String(255))
+
+
 class User(db.Model, UserMixin):
 
-    user_name = db.Column(db.String, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
+    active = db.Column(db.Boolean)
+    roles = db.relationship(Role, secondary=roles_users)
 
 
-user_datastore = SQLAlchemyUserDataStore(db, User)
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 admin.add_view(ModelView(NewsItem, db.session))
-
-if __name__ == '__main__':
-    app.run(debug=True)
