ab6946627cc184b243bc5417db6660f7492f48cf
Fayaz Yusuf Khan <fayaz@weavedin.com>
2016-01-24 15:00:32 +0530

    remove app specific code from manager module

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 from flask.ext.script import Manager
-from hackernews import HackerNews
 
-from reverse_hn_feed.app import app, db, NewsItem, User, UserNewsItem
+from reverse_hn_feed.app import app, db, sync_with_hacker_news
 
 
 manager = Manager(app)
@@ -9,22 +8,7 @@ manager = Manager(app)
 
 @manager.command
 def sync():
-    hn = HackerNews()
-    for story_id in hn.top_stories(limit=90):
-        story = hn.get_item(story_id)
-        persisted_news_item = NewsItem.query.get(story_id)
-        if persisted_news_item:
-            print "Updating story:", story_id
-            persisted_news_item.upvotes = story.score
-        else:
-            print "Adding story:", story_id
-            news_item = NewsItem(
-                id=story_id, url=story.url, posted_on=story.submission_time,
-                upvotes=story.score)
-            db.session.add(news_item)
-            for user in User.query.all():
-                db.session.add(UserNewsItem(user=user, news_item=news_item))
-    db.session.commit()
+    sync_with_hacker_news()
 
 
 @manager.command
